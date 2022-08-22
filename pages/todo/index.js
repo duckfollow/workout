aec2f02af65f80367d9cbc296029eddd08200bd4
OTGprasit <prasit.s@8interactive.co.th>
2022-08-22 22:38:41 +0700

    update todo list

--- pages/todo/index.js
+++ pages/todo/index.js
@@ -5,9 +5,14 @@ import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { NoSsr } from "@mui/material";
 import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import axios from "axios";
+import Collapse from '@mui/material/Collapse';
+import AddIcon from '@mui/icons-material/Add';
 
 
-const Todo = () => {
+const Todo = ({ data, userId }) => {
     // fake data generator
     const getItems = (count, offset = 0) =>
         Array.from({ length: count }, (v, k) => k).map(k => ({
@@ -17,6 +22,12 @@ const Todo = () => {
 
     const [items, setItems] = useState(getItems(10));
     const [selected, setSelected] = useState(getItems(5, 10));
+    const [groupName, setGroupName] = useState("");
+    const [groupList, setGroupList] = useState(data);
+    const [expanded, setExpanded] = useState({});
+    const [groupId, setGroupId] = useState(0);
+    const [todo, setTodo] = useState("");
+
     const id2List = {
         droppable: 'items',
         droppable2: 'selected'
@@ -57,7 +68,8 @@ const Todo = () => {
         margin: `0 0 ${grid}px 0`,
 
         // change background colour if dragging
-        background: isDragging ? "lightgreen" : "grey",
+        background: isDragging ? "lightgreen" : "#ebe9e9",
+        borderRadius: "5px",
 
         // styles we need to apply on draggables
         ...draggableStyle
@@ -66,7 +78,6 @@ const Todo = () => {
     const getListStyle = isDraggingOver => ({
         background: isDraggingOver ? "lightblue" : "lightgrey",
         minHeight: 80,
-        borderRadius: 5,
         padding: grid,
         width: 280
     });
@@ -120,86 +131,259 @@ const Todo = () => {
         }
     };
 
+    const onDragEndTest = result => {
+        const { source, destination } = result;
+
+        let groupId1 = source.droppableId.split("-")[1];
+        let groupId2 = destination.droppableId.split("-")[1];
+        let todoId = groupList.data.find((item) => item.id == groupId1).todo_lists[source.index].id;
+        var new_index = 0;
+        if (!destination) {
+            return;
+        }
+
+        if (source.droppableId === destination.droppableId) {
+            let data = groupList
+            let index = data.data.findIndex((item) => item.id == groupId1);
+
+            const _items = reorder(
+                data.data[index].todo_lists,
+                source.index,
+                destination.index
+            );
+
+            new_index = _items.findIndex((item) => item.id == todoId);
+
+            data.data[index].todo_lists = _items;
+            setGroupList(groupList => ({ ...groupList, ...data }));
+        } else {
+            let data = groupList
+            let index = data.data.findIndex((item) => item.id == groupId1);
+            let index2 = data.data.findIndex((item) => item.id == groupId2);
+
+            const result = move(
+                data.data[index].todo_lists,
+                data.data[index2].todo_lists,
+                source,
+                destination
+            );
+
+            new_index = result[`droppable-${groupId2}`].findIndex((item) => item.id == todoId);
+
+            data.data[index].todo_lists = result[`droppable-${groupId1}`];
+            data.data[index2].todo_lists = result[`droppable-${groupId2}`];
+            setGroupList(groupList => ({ ...groupList, ...data }));
+        }
+        // console.log(groupId1, todoId);
+        console.log(groupList.data);
+        updateTodoAll(groupList.data)
+        // updateTodo(todoId, groupId2, new_index);
+    }
+
+    const createGroup = () => {
+        axios.post(`${process.env.NEXT_PUBLIC_URL}api/v1/todo/group/create`, {
+            store: userId,
+            name: groupName,
+        }).then((response) => {
+            console.log(response);
+            setGroupName("");
+            readData();
+        }, (error) => {
+            console.log(error);
+        });
+    }
+
+    const readData = () => {
+        axios.post(`${process.env.NEXT_PUBLIC_URL}api/v1/todo/list/getall`, {
+            "store": userId
+        }).then((response) => {
+            console.log(response);
+            setGroupList(response.data);
+        });
+    }
+
+    const createTodo = () => {
+        axios.post(`${process.env.NEXT_PUBLIC_URL}api/v1/todo/list/create`, {
+            store: userId,
+            group: groupId,
+            name: todo,
+        }).then((response) => {
+            setTodo("");
+            readData();
+            resetAddTodo();
+        }, (error) => {
+            console.log(error);
+        });
+    }
+
+    const updateTodo = (id, groupId, index) => {
+        axios.post(`${process.env.NEXT_PUBLIC_URL}api/v1/todo/list/update`, {
+            id: id,
+            group: groupId,
+            store: userId,
+            index: index,
+        }).then((response) => {
+            // readData();
+        }, (error) => {
+            console.log(error);
+        });
+    }
+
+    const updateTodoAll = (data) => {
+        axios.post(`${process.env.NEXT_PUBLIC_URL}api/v1/todo/list/update/all`, data).then((response) => {
+            // readData();
+            resetAddTodo();
+        }, (error) => {
+            console.log(error);
+        });
+    }
+
+    const resetAddTodo = () => {
+        let _expanded = expanded;
+        for (const [key, value] of Object.entries(_expanded)) {
+            _expanded[key] = false;
+        }
+        setExpanded(expanded => ({
+            ...expanded,
+            ..._expanded
+        }));
+    }
+
     return (
-        <div style={
-            {
-                padding: 20,
-            }
-        }>
+        <div>
             <Head>
                 <title>Todo</title>
-                <meta name="description" content="Generated by create next app" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/logo-2.png" />
+                <meta property="og:url" content="https://workout.duckfollow.co/" />
+                <meta property="og:type" content="article" />
+                <meta property="og:image:width" content="500" />
+                <meta property="og:image:height" content="500" />
+                <meta property="og:title" content="POS ออนไลน์" />
+                <meta property="og:description" content="POS ออนไลน์" />
+                <meta property="og:image" content="/thumbnail.png" />
+
+                <meta name="twitter:title" content="POS ออนไลน์" />
+                <meta name="twitter:description" content="POS ออนไลน์" />
+                <meta name="twitter:image" content="/thumbnail.png" />
+                <meta name="twitter:card" content="summary_large_image"></meta>
+
+                <meta name="description" content="POS ออนไลน์" />
             </Head>
-            <h1>Todo</h1>
-            <NoSsr>
-                <DragDropContext onDragEnd={onDragEnd}>
-                    <Stack direction="row" spacing={2}>
-                        <div>
-                            <Droppable droppableId="droppable">
-                                {(provided, snapshot) => (
-                                    <div
-                                        ref={provided.innerRef}
-                                        style={getListStyle(snapshot.isDraggingOver)}>
-                                        {items.map((item, index) => (
-                                            <Draggable
-                                                key={item.id}
-                                                draggableId={item.id}
-                                                index={index}>
-                                                {(provided, snapshot) => (
-                                                    <div
-                                                        ref={provided.innerRef}
-                                                        {...provided.draggableProps}
-                                                        {...provided.dragHandleProps}
-                                                        style={getItemStyle(
-                                                            snapshot.isDragging,
-                                                            provided.draggableProps.style
-                                                        )}>
-                                                        {item.content}
-                                                    </div>
-                                                )}
-                                            </Draggable>
-                                        ))}
-                                        {provided.placeholder}
+            <div style={
+                {
+                    minWidth: '100vw',
+                    minHeight: '100vh',
+                    overflowX: 'scroll',
+                    padding: 20,
+                }
+            }>
+                <h1>Todo</h1>
+                <NoSsr>
+                    <DragDropContext onDragEnd={onDragEndTest}>
+                        <Stack direction="row" spacing={2}>
+
+                            {groupList.data.map((group, index) => {
+                                return (
+                                    <div key={group.id}>
+                                        <div className={styles.header_list}>
+                                            <span>{group.name}</span>
+                                        </div>
+                                        <Droppable droppableId={`droppable-${group.id}`} key={index}>
+                                            {(provided, snapshot) => (
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    style={getListStyle(snapshot.isDraggingOver)}
+                                                    {...provided.droppableProps}
+                                                >
+                                                    {group.todo_lists.map((item, _index) => (
+                                                        <Draggable key={`item${group.id}-${item.id}`} draggableId={`item${group.id}-${item.id}`} index={_index}>
+                                                            {(provided, snapshot) => (
+                                                                <div
+                                                                    ref={provided.innerRef}
+                                                                    {...provided.draggableProps}
+                                                                    {...provided.dragHandleProps}
+                                                                    style={getItemStyle(
+                                                                        snapshot.isDragging,
+                                                                        provided.draggableProps.style
+                                                                    )}
+                                                                >
+                                                                    {item.name}
+                                                                </div>
+                                                            )}
+                                                        </Draggable>
+                                                    ))}
+                                                    {provided.placeholder}
+                                                </div>
+                                            )}
+                                        </Droppable>
+                                        <div className={styles.footer_list}>
+                                            <Button startIcon={<AddIcon />} color="primary" onClick={
+                                                () => {
+                                                    let _expanded = expanded;
+                                                    _expanded[`key${index}`] = expanded[`key${index}`] ? !expanded[`key${index}`] : true;
+                                                    for (const [key, value] of Object.entries(_expanded)) {
+                                                        console.log(`${key}: ${value}`);
+                                                        if (key != `key${index}`) {
+                                                            _expanded[key] = false;
+                                                        }
+                                                    }
+                                                    setExpanded(expanded => ({
+                                                        ...expanded,
+                                                        ..._expanded
+                                                    }));
+                                                    setGroupId(group.id);
+                                                    setTodo('');
+                                                }
+                                            }>Add a card</Button>
+                                            <Collapse in={Boolean(expanded[`key${index}`])} timeout="auto" unmountOnExit>
+                                                <Stack direction="column" spacing={1}>
+                                                    <TextField value={todo} onChange={
+                                                        (e) => {
+                                                            setTodo(e.target.value);
+                                                        }
+                                                    } />
+                                                    <Button variant="outlined" color="primary" onClick={createTodo}>เพิ่ม Todo</Button>
+                                                </Stack>
+                                            </Collapse>
+                                        </div>
                                     </div>
-                                )}
-                            </Droppable>
-                        </div>
-                        <div>
-                            <Droppable droppableId="droppable2">
-                                {(provided, snapshot) => (
-                                    <div
-                                        ref={provided.innerRef}
-                                        style={getListStyle(snapshot.isDraggingOver)}>
-                                        {selected.map((item, index) => (
-                                            <Draggable
-                                                key={item.id}
-                                                draggableId={item.id}
-                                                index={index}>
-                                                {(provided, snapshot) => (
-                                                    <div
-                                                        ref={provided.innerRef}
-                                                        {...provided.draggableProps}
-                                                        {...provided.dragHandleProps}
-                                                        style={getItemStyle(
-                                                            snapshot.isDragging,
-                                                            provided.draggableProps.style
-                                                        )}>
-                                                        {item.content}
-                                                    </div>
-                                                )}
-                                            </Draggable>
-                                        ))}
-                                        {provided.placeholder}
-                                    </div>
-                                )}
-                            </Droppable>
-                        </div>
-                    </Stack>
-                </DragDropContext>
-            </NoSsr>
+                                )
+                            })}
+                            <div style={
+                                {
+                                    minWidth: '250px',
+                                }
+                            }>
+                                <Stack direction="column" spacing={1}>
+                                    <TextField value={groupName} onChange={
+                                        (e) => {
+                                            setGroupName(e.target.value);
+                                        }
+                                    } />
+                                    <Button variant="outlined" color="primary" onClick={createGroup} >เพิ่ม Todo</Button>
+                                </Stack>
+                            </div>
+                        </Stack>
+                    </DragDropContext>
+                </NoSsr>
+            </div>
         </div>
     );
+}
+
+export async function getServerSideProps(context) {
+    const cookies = context.req ? context.req.cookies : '';
+    const userId = cookies.userId !== undefined ? cookies.userId : process.env.NEXT_PUBLIC_USER;
+    const res = await axios.post(`${process.env.NEXT_PUBLIC_URL}api/v1/todo/list/getall`, {
+        "store": userId
+    })
+    const data = await res.data
+    return {
+        props: {
+            data,
+            userId
+        },
+    }
 }
 
 export default Todo;
